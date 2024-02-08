dc21f888b253efd5d882a24d0f223e2311982fec
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2024-02-08 17:10:14 +0700

    Create a function to toggle the additional class

--- app/js/main.js
+++ app/js/main.js
@@ -3,40 +3,26 @@ const $item = document.querySelectorAll('.item');
 $item.forEach(item => {
 	item.addEventListener('mouseenter', event => {
 		let element = event.currentTarget;
-		element.classList.add('hover');
-		console.log('currentTarget', event.currentTarget.className);
-		let nextElement = element.nextElementSibling;
-		let previousElement = element.previousElementSibling;
-		for(let i = 1; i < 5; i++){
-			if(nextElement){
-				nextElement.classList.add(`hover-${i}`);
-				nextElement = nextElement.nextElementSibling;
-			}
-			if(previousElement){
-				previousElement.classList.add(`pre-${i}hover`);
-				previousElement = previousElement.previousElementSibling;
-			}
-		}
+		toggleActiveClass(element, 'add');
 
 		element.addEventListener('mouseleave', () => {
-			element.classList.remove('hover');
-			let nextElement = element.nextElementSibling;
-			let previousElement = element.previousElementSibling;
-			for(let i = 1; i < 5; i++){
-				if(nextElement){
-					nextElement.classList.remove(`hover-${i}`);
-					nextElement = nextElement.nextElementSibling;
-				}
-				if(previousElement){
-					previousElement.classList.remove(`pre-${i}hover`);
-					previousElement = previousElement.previousElementSibling;
-				}
-			}
-			console.clear();
+			toggleActiveClass(element, 'remove');
 		});
-
 	});
-
-
-
 });
+
+function toggleActiveClass(el, mode){
+	el.classList[mode]('hover');
+	let nextElement = el.nextElementSibling;
+	let previousElement = el.previousElementSibling;
+	for(let i = 1; i < 5; i++){
+		if(nextElement){
+			nextElement.classList[mode](`hover-${i}`);
+			nextElement = nextElement.nextElementSibling;
+		}
+		if(previousElement){
+			previousElement.classList[mode](`pre-${i}hover`);
+			previousElement = previousElement.previousElementSibling;
+		}
+	}
+}
--- app/script/main.js
+++ app/script/main.js
@@ -4,35 +4,24 @@ var $item = document.querySelectorAll('.item');
 $item.forEach(function (item) {
   item.addEventListener('mouseenter', function (event) {
     var element = event.currentTarget;
-    element.classList.add('hover');
-    console.log('currentTarget', event.currentTarget.className);
-    var nextElement = element.nextElementSibling;
-    var previousElement = element.previousElementSibling;
-    for (var i = 1; i < 5; i++) {
-      if (nextElement) {
-        nextElement.classList.add("hover-".concat(i));
-        nextElement = nextElement.nextElementSibling;
-      }
-      if (previousElement) {
-        previousElement.classList.add("pre-".concat(i, "hover"));
-        previousElement = previousElement.previousElementSibling;
-      }
-    }
+    toggleActiveClass(element, 'add');
     element.addEventListener('mouseleave', function () {
-      element.classList.remove('hover');
-      var nextElement = element.nextElementSibling;
-      var previousElement = element.previousElementSibling;
-      for (var _i = 1; _i < 5; _i++) {
-        if (nextElement) {
-          nextElement.classList.remove("hover-".concat(_i));
-          nextElement = nextElement.nextElementSibling;
-        }
-        if (previousElement) {
-          previousElement.classList.remove("pre-".concat(_i, "hover"));
-          previousElement = previousElement.previousElementSibling;
-        }
-      }
-      console.clear();
+      toggleActiveClass(element, 'remove');
     });
   });
 });
+function toggleActiveClass(el, mode) {
+  el.classList[mode]('hover');
+  var nextElement = el.nextElementSibling;
+  var previousElement = el.previousElementSibling;
+  for (var i = 1; i < 5; i++) {
+    if (nextElement) {
+      nextElement.classList[mode]("hover-".concat(i));
+      nextElement = nextElement.nextElementSibling;
+    }
+    if (previousElement) {
+      previousElement.classList[mode]("pre-".concat(i, "hover"));
+      previousElement = previousElement.previousElementSibling;
+    }
+  }
+}
